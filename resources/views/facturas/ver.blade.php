aaefa7946434e08c1ecee74a169ad8918b95546d
Edgardo Zambrano <eadaggerz@gmail.com>
2022-07-10 20:33:12 -0400

    Fix: now total is displayed at right side of invoices screen, removed reportes button

--- resources/views/facturas/ver.blade.php
+++ resources/views/facturas/ver.blade.php
@@ -83,11 +83,10 @@
                     </tbody>
                 </table>
             </div>
-            </div>
         </div>
         <div class="columns">
             <div class="column">
-                <h5><b>Total Factura: ${{ $factura->getTotal() }}</b></h5>
+                <h5 class="is-size-5 has-text-right"><b>Total:</b> ${{ $factura->getTotal() }}</h5>
             </div>
         </div>
         <div class="columns">
